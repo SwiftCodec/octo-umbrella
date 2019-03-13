a485b7edcd82dedc69ac39b7cc846b8ea8b9f984
DMaier <sshworker@gmail.com>
2019-03-13 18:03:20 +0100

    new

--- ajax.php
+++ ajax.php
@@ -5,6 +5,5 @@ $ajax_peoples = array('firstname' => 'Vasya', 'lastname' => 'Pupkin');
 //echo "Firstname: " . $ajax_peoples['firstname'];
 echo "GET: " . $_GET["w"] . " " . $_GET["h"] . " " . $_GET["d"];
 
-echo "Hello world";
 
 ?>
